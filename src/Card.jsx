64da915a735777d6d249b5bd5f791b5f85ae616a
grz3chotnik <krzysztofkubinski2004@gmail.com>
2025-06-04 13:40:07 -0300

    h1

--- src/Card.jsx
+++ src/Card.jsx
@@ -10,25 +10,25 @@ function Card() {
 
         <div className={"bob"}>
             <img alt={"profile pic"} src={Three} height={50} width={50}/>
-           <h3>grz3chotnik</h3>
+           <h1>grz3chotnik</h1>
             <p className={"bluer"}>Asunción, Paraguay</p>
             <p> yeah</p>
         </div>
         <div className={"bob"}>
-            <h3>my links</h3>
+            <h1>My links</h1>
 
             <a href={"https://ai.2004.lol"} target="_blank">ai.2004.lol</a>
             <a href={"https://terminalcore.org" } target="_blank">terminalcore.org</a>
             <a href={"https://github.com/grz3chotnik"} target="_blank">Github</a>
             <a href={"https://www.youtube.com/@grz3chotnik"} target="_blank">Youtube</a>
             <br/>
-            <h3>other</h3>
+            <h1>Other</h1>
             <a href={"https://discord.com/oauth2/authorize?client_id=1376183588034318346"}target="_blank" >discord bot</a>
 
 <br/>
             {/*<h3>discord bot</h3>*/}
             {/*<Link to="/bot-page">Link</Link> <br/>*/}
-            <p className={"bluer"}>last update: 1 june 2025</p>
+            <p className={"bluer"}>last update: 4 jun 2025</p>
 
         </div>
 
